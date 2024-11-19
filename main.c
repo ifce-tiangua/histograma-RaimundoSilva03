5782522a565d041ab60f07b740fea4a554baca63
RaimundoSilva03 <126274796+RaimundoSilva03@users.noreply.github.com>
2024-11-18 21:52:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,41 +2,49 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void imprime_histograma (int *vendas[], int num_cat) {
-	int i, j;
-	for(i = 0; i < num_cat; i++) {
-	int	vendas_cat = *vendas[i];
-	printf("%d ", vendas_cat);
-	
-	for(j = 0; j < vendas_cat; j++) {
-		
-		printf("*");
-		
-	}
-	printf("\n");
-	}
+void imprime_histograma(int *vendas[], int num_cat) {
+    int i, j;
+    for (i = 0; i < num_cat; i++) {
+        int vendas_cat = *(vendas[i]);
+        printf("%d ", vendas_cat);
+        
+        for (j = 0; j < vendas_cat; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
+
 int main() {
-	setlocale(LC_ALL, "portuguese");
-	int i, num_cat;
-	//printf("Número de categorias: ");
-	scanf("%d", &num_cat);
-	int **vendas = (int **)malloc(num_cat * sizeof(int *));
-	 
-	for( i=0; i< num_cat; i++) {
-	    vendas[i] = (int *)malloc(sizeof(int));
-	    
-		
-		//printf("Número de vendas para categoria %d: ", i+1);
-		scanf("%d", vendas[i]);
-	}
-		//printf("\nHistograma de vendas\n");
-		imprime_histograma(vendas, num_cat);
-		
-		//Deasolocar memória
-		for(i = 0; i < num_cat; i++){
-			free(vendas[i]);
-		}
-			free(vendas);
-	return 0;
+    setlocale(LC_ALL, "portuguese");
+    int i, num_cat;
+
+    scanf("%d", &num_cat);
+
+    int **vendas = (int **)malloc(num_cat * sizeof(int *));
+    if (vendas == NULL) {
+        fprintf(stderr, "Erro ao alocar memória\n");
+        return 1;
+    }
+
+    for (i = 0; i < num_cat; i++) {
+        vendas[i] = (int *)malloc(sizeof(int));
+        if (vendas[i] == NULL) {
+            fprintf(stderr, "Erro ao alocar memória\n");
+            return 1;
+        }
+
+        scanf("%d", vendas[i]);
+    }
+
+    
+    imprime_histograma(vendas, num_cat);
+
+    
+    for (i = 0; i < num_cat; i++) {
+        free(vendas[i]);
+    }
+    free(vendas);
+
+    return 0;
 }
